4cf51d42854cbdef10c2b888180bda22789f2192
AndreyBasatGH <andreybasat@gmail.com>
2026-05-01 11:08:40 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // Премахваме .js, TypeScript ще намери правилния файл сам
 
+// Възклицателният знак тук е правилен за .tsx (казва, че root елементът със сигурност съществува)
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
